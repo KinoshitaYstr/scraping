3ab98c9600cbac78d3aaba6d6ff09c9f5d1b6946
KinoshitaYstr <KinoshitaYstr>
2020-09-08 16:12:02 +0900

    update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const {JSDOM} = require("jsdom");
 
-const url = "http://abehiroshi.la.coocan.jp/";
+const url = "http://abehiroshi.la.coocan.jp/top.htm";
 
 const main = async () => {
     // jsdom飲みのテスト
-    const dom = await JSDOM.fromURL(url);
+    const dom = await JSDOM.fromURL(url, {
+        resources: "usable",
+    });
     console.log(dom.window.document.title);
     console.log(dom.window.document.body.textContent);
-    
+
+    console.log(dom.window.document.body.getElementsByTagName("img"));
+    console.log("jsdomだと中身取りやすいけども画像の取得とかが難しいね???うまくいってるかわからぬ");
+
 }
 
 main();
