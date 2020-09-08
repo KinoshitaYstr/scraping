3514661afcb152ec347a4dfb567bf448a648d1d8
KinoshitaYstr <KinoshitaYstr>
2020-09-08 14:18:44 +0900

    あべひろしのさいとひらけた！！

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello world");
+const poppeteer = require("puppeteer");
+const url = "http://abehiroshi.la.coocan.jp/";
+
+const main = async () => {
+    // ブラウザ設定？？
+    const browser = await poppeteer.launch({
+        headless: false,
+        slowMo: 250,
+        defaultViewport: null,
+    });
+    // ブラウザ開くための新しいページの作成？
+    const page = await browser.newPage();
+    // ひらく
+    await page.goto(url);
+}
+
+main();
